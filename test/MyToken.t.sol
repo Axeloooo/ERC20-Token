dc304b47da05edeb6e4637ba4e87801af7b1f3f1
Axel Omar Sanchez Peralta <axelshz@gmail.com>
2025-09-01 13:34:42 -0600

    feat: Rename ManualToken to MyToken and add comprehensive test cases

--- test/MyToken.t.sol
+++ test/MyToken.t.sol
@@ -2,7 +2,41 @@
 pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
+import {MyTokenScript} from "../script/MyToken.s.sol";
+import {MyToken} from "../src/MyToken.sol";
 
 contract MyTokenTest is Test {
-    function setUp() public {}
+    MyToken public token;
+    MyTokenScript public deployer;
+
+    address bob = makeAddr("bob");
+    address alice = makeAddr("alice");
+
+    uint256 public constant STARTING_BALANCE = 100 ether;
+
+    function setUp() public {
+        deployer = new MyTokenScript();
+        token = deployer.run();
+
+        vm.prank(msg.sender);
+        token.transfer(bob, STARTING_BALANCE);
+    }
+
+    function testBobBalance() public view {
+        assertEq(STARTING_BALANCE, token.balanceOf(bob));
+    }
+
+    function testAllowancesWorks() public {
+        uint256 initialAllowance = 1000;
+
+        vm.prank(bob);
+        token.approve(alice, initialAllowance);
+
+        uint256 transferAmount = 500;
+        vm.prank(alice);
+        token.transferFrom(bob, alice, transferAmount);
+
+        assertEq(token.balanceOf(alice), transferAmount);
+        assertEq(token.balanceOf(bob), STARTING_BALANCE - transferAmount);
+    }
 }
